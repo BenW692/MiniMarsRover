d0f4a9c7b262844cac16579e127ca6579e9eeaa2
BenW692 <brw72@byu.edu>
2025-03-15 16:11:05 -0600

    Nav Pic new word

--- Milestone6.X/LineFollowingNavPic2.c
+++ Milestone6.X/LineFollowingNavPic2.c
@@ -10,6 +10,8 @@
 
 #pragma config FNOSC = FRCDIV // 8 MHz osccilator 
 #pragma config ICS = PGx3 // defines pins 9 and 10 for debug & programming
+#pragma config OSCIOFNC = OFF
+#pragma config SOSCSRC = DIG
 
 #define TRUE 1
 #define FALSE 0
@@ -31,19 +33,24 @@
 #define QRD_HIGH 682 // 2/3 of 1023
 #define QRD_MED 341  // 1/3 of 1023
 
-#define WORD1BIT1 _LATB13
-#define WORD1BIT2 _LATB12
-#define WORD1BIT3 _LATA6
-
-#define WORD2BIT1 _LATB9
-#define WORD2BIT2 _LATB8
-#define WORD2BIT3 _LATB7
+#define WORDBIT1 _LATB4
+#define WORDBIT2 _LATA4 
+#define WORDBIT3 _LATB8
+#define WORDBIT4 _LATB7
 
 int lineState = STRAIGHT;
 
 int main(void) {
     pinSetup();
     config_ADC();
+    
+    while(1)
+    {
+        WORDBIT1 = 1;
+        WORDBIT2 = 1;
+        WORDBIT3 = 0;
+        WORDBIT4 = -1;
+    }
         
     while(TRUE) {
         lineState = senseLine();
@@ -141,16 +148,17 @@ void lineFSM() {
 }
 
 void sendWord1(int s1, int s2, int s3) {
-    WORD1BIT1 = s1;
-    WORD1BIT2 = s2;
-    WORD1BIT3 = s3;
+    WORDBIT1 = s1;
+    WORDBIT2 = s2;
+    WORDBIT3 = s3;
+    //WORDBIT4 = s4;
 }
 
-void sendWord2(int s1, int s2, int s3) {
-    WORD2BIT1 = s1;
-    WORD2BIT2 = s2;
-    WORD2BIT3 = s3;
-}
+//void sendWord2(int s1, int s2, int s3) {
+//    WORD2BIT1 = s1;
+//    WORD2BIT2 = s2;
+//    WORD2BIT3 = s3;
+//}
 
 void pinSetup() {
     /* Digital output pins: 11, 12, 13, 14, 15, 16 
